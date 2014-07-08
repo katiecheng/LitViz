d7cb78b1657d8a551b731af2edcfb694ef082b6a
Hackbright Student <info@hackbrightacademy.com>
2014-07-07 17:46:14 -0700

    Week 6 Day 1, end of day commit, working on search & viz button routing

---
+++ litviz.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask import Response
+from flask import request
+from flask import render_template
+from flask import redirect
+from flask import session
+
+# Create a Flask web app
+app = Flask(__name__)
+app.secret_key = '\xf5!\x07!qj\xa4\x08\xc6\xf8\n\x8a\x95m\xe2\x04g\xbb\x98|U\xa2f\x03'
+
+# Render the home page
+@app.route("/", methods=["GET"])
+def index():
+    session["search_text"]=''
+    return render_template("index.html")
+
+@app.route("/results", methods=["POST"])
+def results():
+    if session.get("search_text"):
+        search_text = session.get("search_text")
+    else:
+        session["search_text"] = request.form.get("search_text")
+        search_text = session.get("search_text")
+    return render_template("results.html", 
+                            search_text = search_text)
+
+@app.route("/viz", methods=["POST"])
+def viz():
+    search_text = session.get("search_text")
+    return render_template("viz.html", 
+                            search_text = search_text)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    session["search_text"] = request.form.get("search_text")
+    if request.form['btn'] == "Search":
+        return redirect("/results")
+    elif request.form['btn'] == "Visualize Data!":
+        return redirect("/viz")
+
+if __name__ == '__main__':
+    # Note that in production, you would want to disable debugging
+    app.run(debug=True)
