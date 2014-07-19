deea1b337e68767ec3cfd253fb36f02ec9877603
Hackbright Student <info@hackbrightacademy.com>
2014-07-18 18:19:34 -0700

    Week 7 Day 5 end of day at HBHQ, added queries for data viz

--- query.py
+++ query.py
@@ -19,8 +19,13 @@ def get_pub_references(pub_id):
 	refs = pub.references
 	return refs
 
-def get_pub_referenced_by():
-	pass
+def get_pub_referenced_by(pub_id):
+	session = model.session
+
+	ref_by = session.query(model.references).filter_by(child_pub_id=pub_id)
+	ref_by_ids = [ref.parent_pub_id for ref in ref_by]
+	return ref_by_ids
+
 
 def get_pub_authors(pub_id):
 	session = model.session
@@ -40,6 +45,14 @@ def get_pub_descriptors(pub_id):
 	descs = [session.query(model.Descriptor).get(desc_id) for desc_id in pub_desc_id_list]
 	return descs
 
+def get_pub_desc_ids(pub_id):
+	session = model.session
+
+	pub = session.query(model.Publication).get(pub_id)
+	pub_desc_list = pub.pub_desc
+	pub_desc_id_list = [pub_desc.desc_id for pub_desc in pub_desc_list]
+	return pub_desc_id_list
+
 def get_pub_keywords():
 	pass
 
@@ -54,6 +67,16 @@ def get_auth_publications(auth_id):
 	pub_auth_list = auth.pub_auth
 	pub_auth_id_list = [pub_auth.pub_id for pub_auth in pub_auth_list]
 	pubs = [session.query(model.Publication).get(pub_id) for pub_id in pub_auth_id_list]
+	return pubs
+
+### GET DESCRIPTOR INFO
+def get_desc_publications(desc_id):
+	session = model.session
+
+	desc = session.query(model.Descriptor).get(desc_id)
+	pub_desc_list = desc.pub_desc
+	pub_desc_id_list = [pub_desc.pub_id for pub_desc in pub_desc_list]
+	pubs = [session.query(model.Publication).get(pub_id) for pub_id in pub_desc_id_list]
 	return pubs
 
 ### GET KEYWORD INFO
@@ -72,3 +95,35 @@ def get_inst_authors():
 def get_dept_authors():
 	pass
 
+
+### GET RELATED PUBLICATIONS
+
+
+def get_related_pubs(pub_id):
+	related_pubs = {}
+
+	# add number of shared descriptors
+	descriptor_ids = get_pub_desc_ids(pub_id)
+	for desc_id in descriptor_ids:
+		for pub in get_desc_publications(desc_id):
+			if pub.id != pub_id:
+				if related_pubs.get(pub.id):
+					related_pubs[pub.id]["relatedness"] += 1
+				else:
+					related_pubs[pub.id] = {"year":pub.year, "title":pub.title, "relatedness":1}
+	
+
+	return related_pubs
+
+def get_shared_child_pub(pub_id_1, pub_id_2):
+
+	pub1_references = get_pub_references(pub_id_1)
+	pub2_references = get_pub_references(pub_id_2)
+
+	in_both = []
+
+	for ref in pub1_references:
+		if ref in pub2_references:
+			in_both.append(ref)
+
+	return in_both
